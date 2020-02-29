62775229c258109325a23aff450a783ce7d8aeeb
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-02-29 13:52:08 +0100

    setup

--- Jose/deserializing.py
+++ Jose/deserializing.py
@@ -1,8 +1,8 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, INCLUDE, EXCLUDE
 
 
 class BookSchema(Schema):
-    title = fields.Str()
+    title = fields.Str(required=True)
     author = fields.Str()
 
 
@@ -12,7 +12,7 @@ incoming_book_data = {
     "description": "A book about writing cleaner code, with examples in Java",
 }
 
-book_schema = BookSchema()
+book_schema = BookSchema(unknown=EXCLUDE)
 book = book_schema.load(incoming_book_data)
 
 print(book)
