49dc1d9169173a4b10983d6da8a96c53d784fe19
Paweł Benkowski <pawlo97.pb@gmail.com>
2020-02-29 16:57:09 +0100

    Flask migration complete

--- Jose/advanced_marshmallow/app.py
+++ Jose/advanced_marshmallow/app.py
@@ -8,6 +8,8 @@ from blacklist import BLACKLIST
 from db import db
 from ma import ma
 from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
@@ -37,6 +39,10 @@ def check_if_token_in_blacklist(decrypted_token):
     return decrypted_token["jti"] in BLACKLIST
 
 
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
 api.add_resource(UserRegister, "/register")
 api.add_resource(User, "/user/<int:user_id>")
 api.add_resource(UserLogin, "/login")
---
+++ Jose/advanced_marshmallow/schemas/item.py
@@ -0,0 +1,12 @@
+# Local
+from ma import ma
+from models.item import ItemModel
+from models.store import StoreModel
+
+
+class ItemSchema(ma.ModelSchema):
+    class Meta:
+        model = ItemModel
+        load_only = ("store",)
+        dump_only = ("id",)
+        include_fk = True
